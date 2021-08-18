8cf2282f0196429c60b6bae755db6b9d4d0ba690
Atis Elsts <atis.elsts@gmail.com>
2021-08-18 20:30:11 +0300

    add full liquidity range query example

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # Overview
 
 See the technical note "Liquidity Math in Uniswap v3" and the [Uniswap v3 whitepaper](https://uniswap.org/whitepaper-v3.pdf) for the description of the purpose of this code.
+
+## Contents
+
+* `uniswap-v3-liquidity-math.py` - implmentations of the equations and examples fromn the "Liquidity Math in Uniswap v3" paper
+* `subgraph-liquidity-query-example.py` - shows the amounts locked in the current tick of the USDC/WETH 0.3% pool
+* `subgraph-liquidity-range-example.py` - shows the amounts locked in all ticks with nonzero liquidity delta in the USDC/WETH 0.3% pool
+
+## Example outputs
+
+### Example output of `uniswap-v3-liquidity-math.py`
+
+Example 1: how much of USDC I need when providing 2 ETH at this price and range?
+amount of USDC y=5076.10
+p_a=1500.00 (75.00% of P), p_b=2500.00 (125.00% of P)
+
+Example 2: I have 2 ETH and 4000 USDC, range top set to 3000 USDC. What's the bottom of the range?
+lower bound of the price p_a=1333.33
+
+Example 3: Using the position created in Example 2, what are asset balances at 2500 USDC per ETH?
+Amount of ETH x=0.85 amount of USDC y=6572.89
+delta_x=-1.15 delta_y=2572.89
+Amount of ETH x=0.85 amount of USDC y=6572.89
+
+
+### Example output of `subgraph-liquidity-query-example.py`
+
+L=22510401004259913887
+tick=195879
+Current price: 0.000321 WETH for 1 USDC (3115.361406 USDC for 1 WETH)
+Amounts at the current tick: 3770791.99 USDC or 1209.30 WETH
+
+
+### Example output of `subgraph-liquidity-range-example.py`
+
+...
+tick=195660 price=3184.336897 USDC for WETH
+        1193.68 WETH locked (potentially worth 3789699.28 USDC)
+tick=195720 price=3165.289029 USDC for WETH
+        1199.90 WETH locked (potentially worth 3786639.86 USDC)
+tick=195780 price=3146.355100 USDC for WETH
+        1218.77 WETH locked (potentially worth 3823192.07 USDC)
+tick=195840 price=3127.534429 USDC for WETH
+        Current tick, both assets present!
+        1332170.50 USDC and 781.24 WETH remaining in the current tick range
+        potentially 3770791.99 USDC or 1209.30 WETH in total in the current tick range
+tick=195900 price=3108.826338 USDC for WETH
+        3748424.25 USDC locked (potentially worth 1209.36 WETH)
+tick=195960 price=3090.230154 USDC for WETH
+        3782324.42 USDC locked (potentially worth 1227.64 WETH)
+tick=196020 price=3071.745208 USDC for WETH
+        3762895.47 USDC locked (potentially worth 1228.68 WETH)
+tick=196080 price=3053.370833 USDC for WETH
+        3740185.70 USDC locked (potentially worth 1228.62 WETH)
+...
---
+++ subgraph-liquidity-range-example.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+#
+# Example that shows the full range of the current liquidity distribution
+# in the 0.3% USDC/ETH pool using data from the Uniswap v3 subgraph.
+#
+
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+POOL_ID = "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8"
+
+TICK_BASE = 1.0001
+
+pool_query = """query {
+  pools (where: {id: "%POOL_ID"}){
+    tick
+    sqrtPrice
+    liquidity
+    feeTier
+    token0 {
+      symbol
+      decimals
+    }
+    token1 {
+      symbol
+      decimals
+    }
+  }
+}"""
+
+tick_query = """query {
+  ticks(skip:%NUM_SKIP, where:{pool:"%POOL_ID"}) {
+    tickIdx
+    liquidityNet
+  }
+}"""
+
+
+def tick_to_price(tick):
+    return TICK_BASE ** tick
+
+# Not all ticks can be initialized. Tick spacing is determined by the pool's fee tier.
+def fee_tier_to_tick_spacing(fee_tier):
+    return {
+        500: 10,
+        3000: 60,
+        10000: 200
+    }.get(fee_tier, 60)
+
+
+client = Client(
+    transport=RequestsHTTPTransport(
+        url='https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
+        verify=True,
+        retries=5,
+    ))
+
+# get pool info
+try:
+    response = client.execute(gql(
+        pool_query.replace("%POOL_ID", POOL_ID)))
+
+    pool = response['pools'][0]
+    current_tick = int(pool["tick"])
+    current_sqrt_price = int(pool["sqrtPrice"]) // (2 ** 96)
+    tick_spacing = fee_tier_to_tick_spacing(int(pool["feeTier"]))
+
+    token0 = pool["token0"]["symbol"]
+    token1 = pool["token1"]["symbol"]
+    decimals0 = int(pool["token0"]["decimals"])
+    decimals1 = int(pool["token1"]["decimals"])
+except Exception as ex:
+    print("got exception", ex)
+
+# get tick info
+tick_mapping = {}
+num_skip = 0
+try:
+    while True:
+        response = client.execute(gql(
+            tick_query.replace("%NUM_SKIP", str(num_skip)).replace("%POOL_ID", POOL_ID)))
+        if len(response["ticks"]) == 0:
+            break
+        num_skip += len(response["ticks"])
+        for item in response["ticks"]:
+            tick_mapping[int(item["tickIdx"])] = int(item["liquidityNet"])
+except Exception as ex:
+    print(ex)
+
+    
+# Start from zero; if we were iterating from the current tick, would start from the pool's total liquidity
+liquidity = 0
+
+# Find the boundaries of the price range
+min_tick = min(tick_mapping.keys())
+max_tick = max(tick_mapping.keys())
+
+current_range_bottom_tick = current_tick // tick_spacing * tick_spacing
+
+# Iterate over the tick map starting from the bottom
+tick = min_tick
+while tick <= max_tick:
+    liquidity_delta = tick_mapping.get(tick, 0)
+    liquidity += liquidity_delta
+
+    # to make the output shorter, skip ticks with no open/close positions
+    # (comment this out to see the full range!)
+    if liquidity_delta == 0:
+        tick += tick_spacing
+        continue
+
+    # Compute square roots of prices corresponding to the bottom and top ticks
+    bottom_tick = tick
+    top_tick = tick + tick_spacing
+    sa = tick_to_price(bottom_tick // 2)
+    sb = tick_to_price(top_tick // 2)
+
+    price = tick_to_price(tick)
+    adjusted_price = price / (10 ** (decimals1 - decimals0))
+    print("tick={} price={:.6f} {} for {}".format(tick, 1 / adjusted_price, token0, token1))
+
+    # Compute the real amounts of both tokens potentially present in the range
+    amount0 = liquidity * (sb - sa) / (sa * sb)
+    amount1 = liquidity * (sb - sa)
+
+    adjusted_amount0 = amount0 / 10 ** decimals0
+    adjusted_amount1 = amount1 / 10 ** decimals1
+
+    if tick < current_range_bottom_tick:
+        # Only asset1 locked
+        print("        {:.2f} {} locked (potentially worth {:.2f} {})".format(adjusted_amount1, token1, adjusted_amount0, token0))
+
+    elif tick == current_range_bottom_tick:
+        # Both locked; compute the real amounts of both
+        print("        Current tick, both assets present!")
+
+        # 1. Print the real amounts of the two assets needed to be swapped to move out of the current tick range
+        amount0actual = liquidity * (sb - current_sqrt_price) / (current_sqrt_price * sb)
+        amount1actual = liquidity * (current_sqrt_price - sa)
+        adjusted_amount0actual = amount0actual / 10 ** decimals0
+        adjusted_amount1actual = amount1actual / 10 ** decimals1
+
+        print("        {:.2f} {} and {:.2f} {} remaining in the current tick range".format(
+            adjusted_amount0actual, token0, adjusted_amount1actual, token1))
+
+        # 2. Print the real amounts of the two assets that would be locked if the tick was only asset0 or asset1
+        print("        potentially {:.2f} {} or {:.2f} {} in total in the current tick range".format(
+            adjusted_amount0, token0, adjusted_amount1, token1))
+
+
+    else:
+        # Only asset0 locked
+        amount0 = liquidity * (sb - sa) / (sa * sb)
+        adjusted_amount0 = amount0 / 10 ** decimals0
+        print("        {:.2f} {} locked (potentially worth {:.2f} {})".format(adjusted_amount0, token0, adjusted_amount1, token1))
+
+    tick += tick_spacing
